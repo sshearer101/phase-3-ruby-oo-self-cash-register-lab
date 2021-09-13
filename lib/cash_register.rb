da1bbc3d612b5e3e1f6a7fd233dfb9dee035d0fc
Sam Shearer <sshearer101@gmail.com>
2021-09-13 11:43:39 -0600

    commit tree 208d25c522274e96d107b055fd17f6e9e178d7a0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :transaction
+
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item (title, price, quantity=1)
+        self.transaction = price * quantity 
+        self.total += self.transaction
+    end
+
+    def apply_discount
+    end
+
+end
